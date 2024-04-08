055247672a73eba2123f0a2e7156b369e2a8e907
Satzvel <164830312+Satzvel@users.noreply.github.com>
2024-04-08 11:37:18 -0700

    Update Main.cpp
    New changes

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,13 @@ int func()
   return a + 10;
 }
 
-int func_multipliers_with_arguments_test(int num1, int num2)
+int func_multiplier(int num1, int num2)
 {
   return num1 * num2;
 }
 int main()
 {
-  print("Hello Swimm\n");
+  print("Hello ASML\n");
   func();
 }
 
